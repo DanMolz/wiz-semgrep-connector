86795c8715f7aec0aadc22a51de75f65277d9dcc
Daniel Moloney <24286010+DanMolz@users.noreply.github.com>
2024-11-19 07:35:29 +1100

    New Build 1.4.0

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -210,8 +210,11 @@ func transformFindings(wizCloudResources wiz.WizCloudResources, semgrepFindings 
 			},
 		}
 
-		// Append the Wiz Findings data structure to the list
-		wizFindings.DataSources = append(wizFindings.DataSources, wizFindingDataSources)
+		// Check if the CWE is supported
+		if isSupportedCWE(strings.Split(finding.Rule.CWE[0], ":")[0]) {
+			// Append the Wiz Findings data structure to the list
+			wizFindings.DataSources = append(wizFindings.DataSources, wizFindingDataSources)
+		}
 	}
 
 	// Log the number of data sources
@@ -239,4 +242,40 @@ func splitRuleName(input string) string {
 		return input[:lastDotIndex]
 	}
 	return input
+}
+
+func isSupportedCWE(cwe string) bool {
+	supportedCWEs := []string{
+		"CWE-17", "CWE-18", "CWE-19", "CWE-20", "CWE-21", "CWE-22", "CWE-59", "CWE-74", "CWE-77", "CWE-78",
+		"CWE-79", "CWE-80", "CWE-88", "CWE-89", "CWE-90", "CWE-91", "CWE-93", "CWE-113", "CWE-115", "CWE-116",
+		"CWE-119", "CWE-120", "CWE-121", "CWE-122", "CWE-123", "CWE-124", "CWE-125", "CWE-126", "CWE-129",
+		"CWE-130", "CWE-131", "CWE-134", "CWE-146", "CWE-170", "CWE-172", "CWE-178", "CWE-182", "CWE-183",
+		"CWE-184", "CWE-185", "CWE-189", "CWE-190", "CWE-191", "CWE-193", "CWE-195", "CWE-200", "CWE-201",
+		"CWE-203", "CWE-208", "CWE-209", "CWE-212", "CWE-228", "CWE-240", "CWE-241", "CWE-244", "CWE-248",
+		"CWE-252", "CWE-254", "CWE-255", "CWE-259", "CWE-264", "CWE-266", "CWE-268", "CWE-269", "CWE-271",
+		"CWE-273", "CWE-274", "CWE-276", "CWE-279", "CWE-281", "CWE-284", "CWE-285", "CWE-287", "CWE-288",
+		"CWE-290", "CWE-294", "CWE-295", "CWE-297", "CWE-300", "CWE-305", "CWE-306", "CWE-307", "CWE-310",
+		"CWE-311", "CWE-312", "CWE-315", "CWE-319", "CWE-320", "CWE-321", "CWE-322", "CWE-326", "CWE-327",
+		"CWE-329", "CWE-330", "CWE-331", "CWE-335", "CWE-338", "CWE-345", "CWE-346", "CWE-347", "CWE-349",
+		"CWE-352", "CWE-354", "CWE-358", "CWE-359", "CWE-361", "CWE-362", "CWE-367", "CWE-369", "CWE-377",
+		"CWE-378", "CWE-384", "CWE-385", "CWE-388", "CWE-399", "CWE-400", "CWE-401", "CWE-404", "CWE-407",
+		"CWE-415", "CWE-416", "CWE-417", "CWE-420", "CWE-425", "CWE-426", "CWE-427", "CWE-428", "CWE-434",
+		"CWE-436", "CWE-440", "CWE-444", "CWE-455", "CWE-457", "CWE-459", "CWE-460", "CWE-471", "CWE-475",
+		"CWE-476", "CWE-477", "CWE-494", "CWE-501", "CWE-502", "CWE-521", "CWE-522", "CWE-523", "CWE-525",
+		"CWE-539", "CWE-547", "CWE-552", "CWE-565", "CWE-567", "CWE-601", "CWE-610", "CWE-611", "CWE-613",
+		"CWE-614", "CWE-617", "CWE-639", "CWE-640", "CWE-646", "CWE-657", "CWE-662", "CWE-665", "CWE-667",
+		"CWE-668", "CWE-669", "CWE-670", "CWE-672", "CWE-674", "CWE-680", "CWE-681", "CWE-682", "CWE-693",
+		"CWE-697", "CWE-704", "CWE-706", "CWE-707", "CWE-732", "CWE-754", "CWE-755", "CWE-763", "CWE-770",
+		"CWE-772", "CWE-776", "CWE-784", "CWE-786", "CWE-787", "CWE-798", "CWE-805", "CWE-823", "CWE-824",
+		"CWE-829", "CWE-833", "CWE-834", "CWE-835", "CWE-838", "CWE-842", "CWE-843", "CWE-862", "CWE-863",
+		"CWE-908", "CWE-909", "CWE-913", "CWE-915", "CWE-916", "CWE-917", "CWE-918", "CWE-923", "CWE-924",
+		"CWE-1021", "CWE-1077", "CWE-1188", "CWE-1236", "CWE-1321",
+	}
+
+	for _, supportedCWE := range supportedCWEs {
+		if cwe == supportedCWE {
+			return true
+		}
+	}
+	return false
 }
